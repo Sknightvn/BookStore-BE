e0d247e849fde14ad71ffbff178d729dab623646
kidiezyllex <buitranthienan1111@gmail.com>
2025-12-12 18:16:23 +0700

    Switch email sending to Resend

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,26 +1,27 @@
-const sgMail = require("@sendgrid/mail");
-
-// Prefer lấy từ env, fallback theo request (không khuyến khích để lâu dài)
-const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
-
-sgMail.setApiKey(SENDGRID_API_KEY);
+const { Resend } = require("resend");
+const RESEND_API_KEY = process.env.RESEND_API_KEY;
+const resend = new Resend(RESEND_API_KEY);
 
 const sendEmail = async (to, subject, text) => {
   const fromEmail = process.env.FROM_EMAIL;
   const fromName = process.env.FROM_NAME;
 
+  if (!RESEND_API_KEY) {
+    throw new Error("RESEND_API_KEY chưa được cấu hình");
+  }
+
   if (!fromEmail) {
     throw new Error("FROM_EMAIL chưa được cấu hình");
   }
 
-  const message = {
+  const from = fromName ? `${fromName} <${fromEmail}>` : fromEmail;
+
+  await resend.emails.send({
+    from,
     to,
-    from: fromName ? { email: fromEmail, name: fromName } : fromEmail,
     subject,
     text,
-  };
-
-  await sgMail.send(message);
+  });
 };
 
 module.exports = sendEmail;
